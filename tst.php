418fb896eceed25a9823b9b45d8052331ce3faa2
DaniloAlves <danilobernardino786@gmail.com>
2022-03-23 20:36:16 -0300

    Primeiro Commit

---
+++ tst.php
@@ -0,0 +1,7 @@
+<?php
+  $email= $_POST['email'];
+  $mensagem= $_POST['mensagem'];
+  $to = "danilobernardino786@gmail.com";
+  $assunto = "Mensagem de ".$email.com
+  mail($to,$assunto,$mensagem);
+?>

2e32115f65d94989bf36de33801bfb9581a773e5
DaniloAlves <danilobernardino786@gmail.com>
2022-03-23 20:49:06 -0300

    Adição do contato por e-mail

--- send-mail.php
+++ send-mail.php
@@ -1,7 +1,32 @@
 <?php
-  $email= $_POST['email'];
-  $mensagem= $_POST['mensagem'];
-  $to = "danilobernardino786@gmail.com";
-  $assunto = "Mensagem de ".$email.com
-  mail($to,$assunto,$mensagem);
+  //Variáveis
+  $nome = $_POST['nome'];
+  $email = $_POST['email'];
+  $mensagem = $_POST['mensagem'];
+  $data_envio = date('d/m/Y');
+  $hora_envio = date('H:i:s');
+
+  //Compo E-mail
+  $arquivo = "
+    <html>
+      <p><b>Nome: </b>$nome</p>
+      <p><b>E-mail: </b>$email</p>
+      <p><b>Mensagem: </b>$mensagem</p>
+      <p>Este e-mail foi enviado em <b>$data_envio</b> às <b>$hora_envio</b></p>
+    </html>
+  ";
+  
+  //Emails para quem será enviado o formulário
+  $destino = "edipojoseoliveira@gmail.com";
+  $assunto = "Contato pelo Site";
+
+  //Este sempre deverá existir para garantir a exibição correta dos caracteres
+  $headers  = "MIME-Version: 1.0\n";
+  $headers .= "Content-type: text/html; charset=iso-8859-1\n";
+  $headers .= "From: $nome <$email>";
+
+  //Enviar
+  mail($destino, $assunto, $arquivo, $headers);
+  
+  echo "<meta http-equiv='refresh' content='10;URL=../contato.html'>";
 ?>

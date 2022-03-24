8ad90fd96079f65f163fea9976c862506b1aed63
DaniloAlves <danilobernardino786@gmail.com>
2022-03-23 21:09:16 -0300

    Novo sistema de e-mail

--- tst.php
+++
@@ -1,7 +0,0 @@
-<?php
-  $email= $_POST['email'];
-  $mensagem= $_POST['mensagem'];
-  $to = "danilobernardino786@gmail.com";
-  $assunto = "Mensagem de ".$email.com
-  mail($to,$assunto,$mensagem);
-?>
